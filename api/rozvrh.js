d0c18772e5f39a90aee2150d2e606466f202a668
Adam Hornof <adam.hornof@gmail.com>
2025-08-31 19:44:17 +0200

    Update cheerio import syntax in rozvrh.js
    Changed the cheerio import to use ES module syntax for compatibility and consistency with modern JavaScript imports.

--- api/rozvrh.js
+++ api/rozvrh.js
@@ -1,5 +1,5 @@
 import fetch from 'node-fetch';
-import cheerio from 'cheerio';
+import * as cheerio from 'cheerio';
 
 export default async function handler(req, res) {
   const url = 'https://sspvc.edupage.org/timetable/view.php?week=2025-W36&student=-465';
